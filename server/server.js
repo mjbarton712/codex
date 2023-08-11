667128aafa3878c2c961a62609e9b09615f44ecd
Matthew Barton <mjbarton712@gmail.com>
2023-08-11 16:56:07 -0500

    update to gpt-4 and use chat history

--- server/server.js
+++ server/server.js
@@ -21,22 +21,57 @@ app.get('/', async (req, res) => {
     })
 });
 
+const conversationHistory = []; // Initialize an empty conversation history array
+conversationHistory.push({ role: "system", content: "You are a helpful assistant." }); // Add system background - can customize later
+
 //post to openai from a text model
 //for pricing details, see - https://openai.com/api/pricing/#faq-fine-tuning-pricing-calculation
 app.post('/', async (req, res) => {
     try {
-        const prompt = req.body.prompt;
+        const userMessage = req.body.prompt;
+        conversationHistory.push({ role: "user", content: userMessage }); // Add user message to conversation history
+        logRoleAndContent("pre", conversationHistory);
         const response = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo", //see GPT models here: https://platform.openai.com/docs/api-reference/chat/create
-            messages: [{role: "user", content: `${prompt}`}],
+            model: "gpt-4", //see GPT models here: https://platform.openai.com/docs/api-reference/chat/create
+            messages: 
+            conversationHistory, // Using entire conversation history in API call so that we can have convos
         })
+
+        // Add bot's response to convo history as well
+        conversationHistory.push({ role: "assistant", content: response.data.choices[0].message.content });
+        logRoleAndContent("post", conversationHistory);
+
         res.status(200).send({
             bot: response.data.choices[0].message.content
         })
     } catch (error) {
-        console.log(error);
-        res.status(500).send({ error });
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+            res.status(error.response.status).send({ error: error.response.data });
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.log(error.request);
+            res.status(500).send({ error: 'No response from the server' });
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+            res.status(500).send({ error: error.message });
+        }
     }
 });
 
 app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
+
+function logRoleAndContent(position, arrayOfObjects) {
+    console.log(position);
+    for (const item of arrayOfObjects) {
+        if (item.hasOwnProperty('role') && item.hasOwnProperty('content')) {
+            console.log(`Role: ${item.role}, Content: ${item.content}`);
+        } else {
+            console.log('Invalid item format:', item);
+        }
+    }
+}
